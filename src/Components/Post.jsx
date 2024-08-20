e7701772ef9f6dee687c76629e5ef7f49dacbe0b
aojha@Shyam <aojha@gmail.com>
2024-08-20 14:54:29 +0530

    add css for tags

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -18,15 +18,29 @@ const Post = ({ post }) => {
           </span>
         </h5>
         <p className="card-text">{post.body}</p>
-        {post.tags?.map((tag) => (
-          <span key={tag} className="badge text-bg-primary has-tags">
-            {tag}
-          </span>
-        ))}
-        <div className="alert alert-success reactions" role="alert">
-          This post has been reacted by{" "}
-          {post.reactions?.likes + post.reactions?.dislikes || 0} people.
-        </div>
+
+        {Array.isArray(post.tags) && post.tags.length > 0 && (
+          <div className="mb-2">
+            {post.tags.map((tag) => (
+              <span key={tag} className="badge text-bg-primary hashtag">
+                {tag}
+              </span>
+            ))}
+          </div>
+        )}
+        {post.reactions && typeof post.reactions === "object" ? (
+          <div className="alert alert-danger reactions" role="alert">
+            <p>This post has:</p>
+            <ul>
+              <li>{post.reactions.likes || 0} likes</li>
+              <li>{post.reactions.dislikes || 0} dislikes</li>
+            </ul>
+          </div>
+        ) : (
+          <div className="alert alert-danger reactions" role="alert">
+            This post has been reacted to by {post.reactions || 0} people
+          </div>
+        )}
       </div>
     </div>
   );
