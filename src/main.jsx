f44457dc9e79ff50511e8e6e98bb83d495568ca2
aojha@Shyam <aojha@gmail.com>
2024-08-22 12:58:32 +0530

    outlet added

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import CreatePost from "./Components/CreatePost.jsx";
+import PostList from "./Components/PostList.jsx";
+import App from "./routes/App.jsx";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <PostList /> },
+      { path: "create-post", element: <CreatePost /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
